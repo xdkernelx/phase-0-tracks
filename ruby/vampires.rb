d3f613c541af573d9594af3f02b728fa770116e2
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-04 11:35:49 -0700

    Added input for employee name.

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+#Nestor Alvarez
+#devBootcamp, 4.4 Solo Challenge
+#20160804
+#vampires.rb - vampire detection technology
+
+# class Employee(name, age, insurance)
+
+# end
+
+name = ""
+age = nil
+year_ob = nil
+likes_garlic = nil
+insurance = false
+
+
+print("What is your name? ")
+name = gets.chomp
+print("Your name is: ", name, "\n")
+
