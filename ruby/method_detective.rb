3e16acebf26a0b6d421619955fc93435a5746fcc
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-07 12:46:22 -0700

    Solved case solved, added print statement.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,7 +4,7 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
- "iNvEsTiGaTiOn".swapcase
+ puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
 # "zom".<???>
