71731564dcbe55f6b4db69e39225ea389e9783a6
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-07 13:05:13 -0700

    Solving 8th case, method: reverse.chop.reverse.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -25,7 +25,7 @@ puts " suspects".rjust(18, 'the usual')
 puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
 # "Elementary,    my   dear        Watson!".<???>
