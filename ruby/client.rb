316b45735befd631e1a521473abdf6815cf40cee
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-08 16:39:33 -0700

    Created a get_hash method that creates a runtime hash of the instance variables and their attributes.

--- ruby/client.rb
+++ ruby/client.rb
@@ -91,7 +91,9 @@ class Client
   end
 
   def get_hash
-
+    hash = {}
+    instance_variables.each {|var| hash[var.to_s.delete("@").to_sym] = instance_variable_get(var) }
+    hash
   end
 
   def print_client
