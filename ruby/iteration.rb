2cde7bb909a64efb13c702ea97eac6502c5ec457
Nestor Alvarez <grimreaper1169@gmail.com>
2016-08-09 16:17:22 -0700

    Practiced .each and .map on hash and array classes.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -3,3 +3,13 @@ letters = {"a" => 10, "b" => 5, "c" => 200}
 
 p an_name
 p letters
+
+an_name.each { |var|
+    puts("#{var}")
+}
+
+letters.each {|key, val| puts("#{key} is #{val}")}
+p letters
+
+an_name = an_name.map {|var| p var.next}
+p an_name
