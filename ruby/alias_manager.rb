60772409bae6ffe879c2361659dfe00988f6785e
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-10 17:24:43 -0700

    Created a robust method that switches variables.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,3 +2,15 @@
 #DBC, Bobolinks 2016
 #5.6 Solo Challenge, 20160810
 
+def switch_names(first, last)
+  return last, first
+end
+
+first = "Nestor"
+last = "Alvarez"
+
+first, last = switch_names(first, last)
+
+puts("First name: #{first}")
+puts("Last name: #{last}")
+
