2fffc7ae01befd3114e538a348a630ccad27dd8b
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-10 17:30:43 -0700

    Creating a vowel? method that returns a boolean for inputs depending if it's a vowel.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,11 +6,26 @@ def switch_names(first, last)
   return last, first
 end
 
+def next_vowel(char)
+
+end
+
+def vowel?(char)
+  return true if ["a", "e", "i", "o", "u"].include?(char.downcase)
+  false
+end
+
 first = "Nestor"
 last = "Alvarez"
+vowels_arr = ["a", "e", "i", "o", "u"]
 
 first, last = switch_names(first, last)
 
 puts("First name: #{first}")
 puts("Last name: #{last}")
 
+puts("Is 'a' a vowel? #{vowel?("b")}")
+
+vowels_arr.each {|val|
+  puts(val)
+}
