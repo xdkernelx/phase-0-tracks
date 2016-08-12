8db30ae89b8ecdfd7fb90f46567c09b072c823d9
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-12 11:09:20 -0700

    Added pseudo-code for Puppy class variables and methods. Ongoing process.

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,3 +1,18 @@
 class Puppy
 
+=begin
+  ***Pseudo-code***
+  Possible characteristics of Puppy class:
+    -breed
+    -name
+    -owner (id, first_name, last_name)
+    -vaccinations {}
+    -personal attributes {} (i.e. fur_color, eye_color, id_marks[], rfid, etc
+
+    -accessor methods
+    -mutator methods
+    -readable output method
+    -hash for database entries
+=end
+
 end
