f49a1c05718bf3a08dfce4cba76181e6ebfbd673
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-12 16:38:39 -0700

    Added initialized Puppy object.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,3 +7,15 @@ class Puppy
 
 end
 
+a = Puppy.new(:breed => "Husky", \
+              :name => "Sparky", \
+              :owner => {last: "Alvarez", first: "Nestor", id: 201}, \
+              :vaccinations => {:flu => true, :smallpox => true}, \
+              :an_attr => {
+                  :fur_color => "brown", :eye_color => "blue", \
+                  :id_marks => ["lower-left scar", "left-eye spot"],
+                  :rfid => 2304
+                }
+              )
+
+p a
