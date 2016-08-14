5b46638d0c16a3198c79aba333cacaedac5c8c59
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-13 17:44:42 -0700

    Added initialize method with default parameters.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender: "none", ethnicity: "none")
     puts("Initializing Santa instance ...")
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
