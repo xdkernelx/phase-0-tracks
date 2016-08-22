cfec4e94d543761c35d1b5afd0bc4505f60a8d84
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-22 02:27:23 -0700

    Added a test for the dividing method.

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -24,4 +24,8 @@ describe Calculator do
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
   end
+
+  it "divides two integers" do
+    expect(calculator.divide(7,4)).to eq 1
+  end
 end
