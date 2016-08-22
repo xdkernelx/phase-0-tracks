283d9a86f642715b94a4cd2960836e62d3c6a176
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-22 03:04:31 -0700

    Testing get_mad_at function.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,4 +18,26 @@ class Santa
     puts("That was a good #{cookie}!")
   end
 
+  def celebrate_birthday
+    @age = @age + 1
+  end
+
+  #Removes the reindeer from any position if it exists, drops it down to the end
+  def get_mad_at(name)
+    if @reindeer_ranking.include?(name.downcase.capitalize)
+      @reindeer_ranking.delete(name)
+      @reindeer_ranking << name.downcase.capitalize
+      return true
+    else
+      puts("#{name.capitalize} does not exist")
+      return false
+    end
+  end
+
+  def get_ranking
+    new_arr = []
+    @reindeer_ranking.each {|val| new_arr << val}
+    return new_arr
+  end
+
 end
