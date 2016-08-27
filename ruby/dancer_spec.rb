d9a97efd9a9f7a3a6513047107e615f97585584d
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-27 07:07:30 -0700

    Crated my_solution.rb, state_data.rb

---
+++ ruby/dancer_spec.rb
@@ -0,0 +1,16 @@
+describe Dancer do
+  let(:dancer) { Dancer.new("Misty Copeland", 33) }
+
+  it "has a readable name" do
+    expect(dancer.name).to eq "Misty Copeland"
+  end
+
+  it "has a readable age" do
+    expect(dancer.age).to eq 33
+  end
+
+  it "has a writeable age" do
+    dancer.age = 34
+    expect(dancer.age).to eq 34
+  end
+end
