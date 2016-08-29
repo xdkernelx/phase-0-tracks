dd79be0d8ddc595759e0a52ecc8cdb11c9964f68
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-29 05:51:41 -0700

    Add documentation.

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,19 @@
+/* 
+    Nestor Alvarez
+    Aug 29, 2016
+*/
+
 var test_arr = ["long phrase","longest phrase","longer phrase"];
 var test_arr2 = ["1", "2", "three", "three", "  three"];
 
+
+/*
+    Straight forward string parsing.
+    We should be able to create a for loop
+    That starts at the last index of the String
+    and continues into the beginning, all while 
+    assigning the character to a new, reversed string
+*/
 function longest_string(str_arr) {
     str_length = 0;
     longest_str = "";
@@ -14,6 +27,13 @@ function longest_string(str_arr) {
     return longest_str;
 }
 
+
+/*
+    Whether the key-value pair exists depends on
+    only one object. We need to iterate through it
+    and simple making a break-return when a pair is
+    found. Returns false by default.
+*/
 function contains_pair(obj1, obj2) {
     for(var key in obj1) {
         if (obj1[key] == obj2[key]) {
@@ -23,6 +43,15 @@ function contains_pair(obj1, obj2) {
     return false;
 }
 
+/*
+    Using nested for-loops, we create "num" number of
+    strings. Inside this strings, we concatenate characters
+    using the standard ASCII and the included fromCharChode
+    function. We use Math.random to our advantage in setting
+    up the range of possible lowercase characters. We push the
+    final concatenated string into the array, and finally
+    return the array reference.
+*/
 function generate_str(num) {
     arr = []
     for(i = 0; i < num; i++) {
