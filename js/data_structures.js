2dfe8d539c2e597bf13336bc1e86db32989ef9ab
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-08-29 04:42:18 -0700

    Populate horse_object with data.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,14 @@
 //Nestor Alvarez
 
-var horses = ["Ed", "Edd", "Eddy", "Ted"]
-var colors = ["red", "white", "blue", "fuchsia"]
+var horses = ["Ed", "Edd", "Eddy", "Ted"];
+var colors = ["red", "white", "blue", "fuchsia"];
+var horse_object = {};
 
-horses.push("Eddie")
-colors.push("green")
+horses.push("Eddie");
+colors.push("green");
+
+for(i = 0; i < horses.length; i++) {
+    horse_object[horses[i]] = colors[i];
+}
+
+console.log(horse_object);
