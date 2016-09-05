04ddd4d1f3d4574d045d9cd6c4a1b7d71a76ccfe
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-09-05 08:12:43 -0700

    Add create command variables.

--- databases/driver.rb
+++ databases/driver.rb
@@ -5,3 +5,27 @@
 require 'sqlite3'
 require 'faker'
 
+#Create SQLite3 Database
+db = SQLite3::Database.new("employees.db")
+db.results_as_hash = true
+
+# learn about fancy string delimiters
+create_table_employees = <<-SQL
+    CREATE TABLE IF NOT EXISTS employees (
+      id INTEGER PRIMARY KEY, 
+      name VARCHAR(255), 
+      emp_id INT, 
+      position VARCHAR(255), 
+      FOREIGN KEY (emp_id) REFERENCES employers(id)
+    )
+SQL
+
+create_table_employees = <<-SQL
+    CREATE TABLE employers (
+      id INTEGER PRIMARY KEY, 
+      name VARCHAR(255), 
+      address VARCHAR(255), 
+      emp_no INT, 
+      aca_compliance BOOLEAN
+    )
+SQL
