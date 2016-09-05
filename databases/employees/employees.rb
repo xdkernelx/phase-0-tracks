06eb7ff626c65e03beb7d652772be07e256dd084
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-09-05 08:46:18 -0700

    Add create_employer method.

--- databases/employees/employees.rb
+++ databases/employees/employees.rb
@@ -8,3 +8,8 @@ def create_employee(db, name, emp_id, position)
     db.execute("INSERT INTO employees (name, emp_id, position) VALUES (?, ?, ?)", [name, emp_id, position])
     return true
 end
+
+def create_employer(db, name, emp_no, aca_compliance)
+    db.execute("INSERT INTO employers (name, emp_no, aca_compliance) VALUES (?, ?, ?)", [name, emp_no, aca_compliance])
+    return true
+end
