6f19a1cbcb3a02cacd38ca20e70399aeb428099a
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-09-11 15:24:25 -0700

    Fix missing dot inside event click class.

--- web_dev/jquery/jquery.js
+++ web_dev/jquery/jquery.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $(this).children('ul').stop(true,false,true).slideToggle(300);
   });
 
-  $('responsive').click(function(){
+  $('.responsive').click(function(){
     alert("You have clicked my picture. It does nothing.");
   });
 
